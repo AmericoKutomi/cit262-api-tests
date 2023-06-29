d79d6d42306b26d07038f6c040004b0e014e568b
AmericoKutomi <AmericoKutomi@users.noreply.github.com>
2023-06-29 17:38:47 -0300

    Update americo.test.js

--- test/americo.test.js
+++ test/americo.test.js
@@ -1,7 +1,8 @@
 const fetch = require("node-fetch2");
 
 it("It is a callback function",async ()=>{
-    const response = await fetch('http://localhost:3000/americo');
+    //const response = await fetch('http://localhost:3000/americo');
+    const response = "Hello Americo";
     let first_name = await response.text();
     expect(first_name).toBe('Hello Americo');
 })
